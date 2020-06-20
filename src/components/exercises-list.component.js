136ffe5dbb54d81aaff6f2712835438aa818d1ed
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-06-19 18:30:15 -0700

    added proxy in package.json for endpoint

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -13,7 +13,7 @@ export default class ExercisesList extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/exercises/')
+    axios.get('/exercises/')
       .then(response => {
         this.setState({
           exercises: response.data
@@ -23,7 +23,7 @@ export default class ExercisesList extends Component {
   }
 
   deleteExercise(id) {
-    axios.delete('http://localhost:5000/exercises/' + id)
+    axios.delete('/exercises/' + id)
       .then(response => { console.log(response.data) });
 
     this.setState({
